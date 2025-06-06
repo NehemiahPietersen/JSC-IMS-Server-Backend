a69fcf0bff8ed33758648851e03b3ead9222b5e8
NehemiahPietersen <nehemiah11pietersen@gmail.com>
2025-06-06 10:09:37 +0200

    implemented model mapper config

---
+++ InventoryManagementSystem/src/main/java/com/example/InventoryManagementSystem/config/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package com.example.InventoryManagementSystem.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        // bean is used to help expose the model Mapper throughout lifecycle of application
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+
+        return modelMapper;
+    }
+}
